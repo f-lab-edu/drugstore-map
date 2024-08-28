4ee78e4653ea3a96b170ff96daacea2fc0f46cf7
JHwan96 <dlwnghks00@gmail.com>
2024-08-28 17:45:49 +0900

    refactor: Api 코드 수정
    - field injection -> constructor injection
    - builder 대신 생성자 사용
    - @PostConstruct 삭제
    - System.out.println 대신 logger 사용
    - 이후에 DTO 통합할 예정

--- openapi/src/main/java/org/healthmap/openapi/api/HospitalApi.java
+++ openapi/src/main/java/org/healthmap/openapi/api/HospitalApi.java
@@ -1,10 +1,9 @@
 package org.healthmap.openapi.api;
 
-import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 import org.healthmap.openapi.config.KeyInfo;
 import org.healthmap.openapi.dto.HospitalDto;
 import org.healthmap.openapi.utility.XmlUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -19,26 +18,26 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-//TODO: key 숨기기
 //TODO: 예외 처리
+@Slf4j
 @Component
 public class HospitalApi {
-    @Autowired
-    KeyInfo keyInfo;
-
-    private final String page = "&pageNo="; //페이지 번호
-    private final int rowSize = 1000;       //한 페이지 결과 수
+    private final KeyInfo keyInfo;
+    private final String page;//페이지 번호
+    private final int rowSize;       //한 페이지 결과 수
     private String url;
 
-    @PostConstruct
-    public void init() {
+    public HospitalApi(KeyInfo keyInfo) {
+        this.keyInfo = keyInfo;
+        this.page = "&pageNo=";
+        this.rowSize = 1000;
         this.url = "http://apis.data.go.kr/B551182/hospInfoServicev2/getHospBasisList"  //URL
                 + "?serviceKey=" + keyInfo.getServerKey()    //Service Key
                 + "&numOfRows=" + rowSize;
     }
 
-
     // TODO: 예외처리
+
     /**
      * 전체 페이지 크기를 반환하는 메서드
      */
@@ -67,23 +66,25 @@ public class HospitalApi {
             Node node = nodeList.item(i);
             if (node.getNodeType() == Node.ELEMENT_NODE) {
                 Element element = (Element) node;
-                HospitalDto hospitalDto = HospitalDto.builder()
-                        .code(XmlUtils.getStringFromElement("ykiho", element))
-                        .name(XmlUtils.getStringFromElement("yadmNm", element))
-                        .address(XmlUtils.getStringFromElement("addr", element))
-                        .phoneNumber(XmlUtils.getPhoneNumberFromElement("telno", element))
-                        .hospitalUrl(getHospitalUrlFromElement(element))
-                        .postNumber(XmlUtils.getStringFromElement("postNo", element))
-                        .typeName(XmlUtils.getStringFromElement("clCdNm", element))
-                        .stateName(XmlUtils.getStringFromElement("sidoCdNm", element))
-                        .cityName(XmlUtils.getStringFromElement("sgguCdNm", element))
-                        .emdongName(XmlUtils.getStringFromElement("emdongNm", element))
-                        .xPos(XmlUtils.getStringFromElement("XPos", element))
-                        .yPos(XmlUtils.getStringFromElement("YPos", element))
-                        .build();
+                String code = XmlUtils.getStringFromElement("ykiho", element);
+                String name = XmlUtils.getStringFromElement("yadmNm", element);
+                String address = XmlUtils.getStringFromElement("addr", element);
+                String phoneNumber = XmlUtils.getPhoneNumberFromElement("telno", element);
+                String hospitalUrl = getHospitalUrlFromElement(element);
+                String typeName = XmlUtils.getStringFromElement("clCdNm", element);
+                String postNumber = XmlUtils.getStringFromElement("postNo", element);
+                String stateName = XmlUtils.getStringFromElement("sidoCdNm", element);
+                String cityName = XmlUtils.getStringFromElement("sgguCdNm", element);
+                String emdongName = XmlUtils.getStringFromElement("emdongNm", element);
+                String xPos = XmlUtils.getStringFromElement("XPos", element);
+                String yPos = XmlUtils.getStringFromElement("YPos", element);
+                HospitalDto hospitalDto = new HospitalDto(
+                        code, name, address, phoneNumber, hospitalUrl, postNumber, typeName, stateName, cityName, emdongName, xPos, yPos
+                );
                 hospitalDtoList.add(hospitalDto);
             }
         }
+        log.info("hospitalDtoList size: {}", hospitalDtoList.size());
         return hospitalDtoList;
     }
 
